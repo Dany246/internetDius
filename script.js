4fe05dfea51a7d77dd0e61f03e684bcaa355566c
Dany246 <145447252+Dany246@users.noreply.github.com>
2024-09-30 11:28:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function ha(){
+    document.getElementById("cim").innerHTML ="Az internet fejlödése napjainkig"
+    document.getElementById("style").innerHTML ='<link id="style" rel="stylesheet" href="1960.css">';
+    document.getElementById("szoveg").innerHTML ="A gyökerek az 1960-as évekig nyúlnak vissza, a történet katonai fejlesztések civil szférába való átszivárgásával kezdödött. Abban az idöben merült föl ugyanis az USA-ban egy kevéssé sebezhetö számítógép-hálózat szükségessége, amelynek egy esetleges atomtámadás után megmaradó részei müködöképesek maradnak. Dwight D. Eisenhower elnök – a szovjetek ürversenybeli sikereit ellensúlyozandó, a Szputnyik–1 fellövésének hírére – elrendelte a DARPA (Defense Advanced Research Project Agency) felállítását, amely a kutatásokat azután finanszírozta. Kidolgoztak egy többközpontú, csomagkapcsolt (ahol az adatok továbbítása kisebb csomagokban történik) hálózati kommunikációs rendszert (az NCP protokollt), amely a mai TCP/IP szabvány ösének tekinthetö. Ezen az elven kezdett müködni 1969-ben az ARPANET (Advanced Research Projects Agency Network), és a katonai felhasználásokon kívül a csomagkapcsolt adattovábbítás további kutatásra szolgált, de egyes egyetemek, katonai bázisok és kormányzati laboratóriumok kutatói is használták elektronikus levelezésre, fájlok cseréjére és távoli bejelentkezésre egymás számítógépei között."
+}
+
+
+function he4(){
+    document.getElementById("cim").innerHTML ="Az internet fejlödése napjainkig"
+    document.getElementById("style").innerHTML ='<link id="style" rel="stylesheet" href="1974.css">';
+    document.getElementById("szoveg").innerHTML="1974-ben jelent meg elöször az „internet” kifejezés, egy a TCP protokollról szóló tanulmányban. 1983-ban az addig szigorúan ellenörzött ARPANET-böl MILNET (Military Network) néven leválasztották a hadászati szegmenst."
+
+
+
+}
+
+function n8(){
+    document.getElementById("cim").innerHTML ="Az internet fejlödése napjainkig"
+    document.getElementById("style").innerHTML = '<link id="style" rel="stylesheet" href="1988.css">'
+    document.getElementById("szoveg").innerHTML="1988 az első hálózaton keresztül terjedő féregvírus (worm) éve volt. A National Science Foundation felismerte, hogy a gerinchálózat döntő fontosságú lehet a tudományos kutatásban, ezért igen nagy szerepet vállalt az internet bővítésében. 1985–86-ban kiépítették az NSF 6 szuperszámítógép központját, és az így kialakult hálózatot (mely az NSFNET nevet kapta) összekapcsolták az ARPANET-tel. Az NSFNET (National Science Foundation Network) több bővítés után (optikai kábeleket fektettek le, újabb vonalakat hoztak létre, aminek eredményeként a hálózat sebessége a kezdeti 56 kbps-ról 45 Mbps-ra emelkedett) ma is az USA domináns gerinchálózata. "
+
+}
+
+
+function k8(){
+     document.getElementById("cim").innerHTML ="Az internet fejlödése napjainkig"
+  document.getElementById("style").innerHTML = '<link id="style" rel="stylesheet" href="1998.css">'
+  document.getElementById("szoveg").innerHTML="998-ra mintegy 300 millióra nőtt a webdokumentumok száma, és a web naponta további másfél millióval bővül.A gyors elterjedésből és egyszerű kezelhetőségből eredeztethető a korábban a felhasználó szempontjából ingyenes, nonprofit hálózat elüzletiesedése is. Az óriási és egyre növekvő, többé már nem csak profi számítástechnikusokból álló internetközösség hatalmas üzleti lehetőségeket rejt. Sőt, az emberek közötti kommunikációs kapcsolatok teljesen új formáit teszi lehetővé."
+
+
+}
+
+function napjaink(){
+    document.getElementById("cim").innerHTML ="Az internet fejlodese napjainkig"
+    document.getElementById("style").innerHTML = '<link id="style" rel="stylesheet" href="napjaink.css">'
+    document.getElementById("szoveg").innerHTML='Manapsag az internetes technologiakat korulbelul negymilliardnyian hasznaljak szemelyes, illetve uzleti celokra, peldaul informaciokeresesre, szolgaltatesok es aruk megrendelesere, kapcsolattartasra, szorakozasra stb.'
+}
